3aa83d44be02b27f3467a7491a11276fb1a9b32f
Md. Ahsanur Rahaman <mdahsanurrahaman2456@gmail.com>
2025-03-21 01:24:03 +0600

    initial commit

---
+++ script.js
@@ -0,0 +1,30 @@
+document.getElementById("generateBtn").addEventListener("click", generatePalette);
+        
+function generatePalette() {
+    const palette = document.getElementById("palette");
+    const numColors = document.getElementById("numColors").value;
+    palette.innerHTML = "";
+    for (let i = 0; i < numColors; i++) {
+        let color = getRandomColor();
+        let colorBox = document.createElement("div");
+        colorBox.className = "color-box";
+        colorBox.style.backgroundColor = color;
+        colorBox.innerText = color;
+        colorBox.addEventListener("click", () => copyToClipboard(color));
+        palette.appendChild(colorBox);
+    }
+}
+
+function getRandomColor() {
+    let letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+function copyToClipboard(color) {
+    navigator.clipboard.writeText(color);
+    alert("Copied: " + color);
+}
